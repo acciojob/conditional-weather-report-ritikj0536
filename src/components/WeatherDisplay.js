234db42db27949c5d6ef2c89d1dd2cfbd675fa34
ritikj0536 <jharitik64@gmail.com>
2025-12-15 13:28:41 +0530

    weatherapp

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const WeatherDisplay = ({ weather }) => {
+  const tempColor = weather.temperature > 20 ? "red" : "blue";
+
+  return (
+    <div>
+      <p>Temperature:{" "}
+        <span style={{ color: tempColor }}>
+          {weather.temperature}
+        </span>
+      </p>
+
+      <p>Conditions: {weather.conditions}</p>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
